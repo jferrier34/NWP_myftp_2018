9081e87bab752d1c7ba0dbe1428314f052edeaf8
jferrier <jferrier@15.2.0.10.rev.sfr.net>
2019-04-14 15:52:57 -0400

    [added] the functions are separeted by 3 files and the .h is ok

---
+++ Src/file2.c
@@ -0,0 +1,76 @@
+/*
+** EPITECH PROJECT, 2019
+** Project name
+** File description:
+** Description
+*/
+
+#include "../include/my.h"
+
+void my_quit(int client)
+{
+    if (send(client, "221 Service closing control connection.\r\n", 41, 0) == -1)
+        exit (0);
+    shutdown(client, 2);
+    exit(0);
+}
+
+char **my_tabb(char *tab)
+{
+    char **my_tab = malloc(sizeof(char *) * 3);
+    my_tab[0] = strtok(tab, "\r ");
+    my_tab[1] = strtok(NULL, "\r ");
+    my_tab[2] = NULL;
+    return (my_tab);
+}
+
+int user_check(int sock, FILE *need)
+{
+    char tab[] = "Anonymous";
+    char **dubl_tab;
+    size_t size = 0;
+    char *dir = NULL;
+    if (getline(&dir, &size, need) == -1)
+        exit (84);
+    dubl_tab = my_tabb(dir);
+    if (strcmp(dubl_tab[0], "USER") != 0) {
+        send(sock, "332 Need account for login.\r\n", 29, 0);
+        return (0);
+    }
+    if (strcmp(dubl_tab[1], tab) == 0) {
+        if(send(sock, "331 User name okay, need password.\r\n", 36, 0) < 0)
+            exit(84);
+        return (1);
+    }
+    return (0);
+}
+
+void command_all(int client, char *recup)
+{
+    char **my_tab;
+    my_tab = my_tabb(recup);
+    if (strcmp(my_tab[0], "QUIT") == 0)
+        my_quit(client);
+    if (strcmp(my_tab[0], "NOOP") == 0)
+        my_noop(client);
+}
+
+void login_user(int sock)
+{
+    int check_user = 0;
+    int check_pass = 0;
+    size_t size = 0;
+    char *recup = NULL;
+    FILE *need = fdopen(sock, "r");
+    check_user = user_check(sock, need);
+    if (check_user == 1)
+        check_pass = password_check(sock, need);
+    if (check_user == 1 && check_pass == 1) {
+        while (getline(&recup, &size, need) != -1)
+            command_all(sock, recup);
+        shutdown(sock, 2);
+        fclose(need);
+        exit(0);
+    }
+    return;
+}
--- include/my.h
+++ include/my.h
@@ -6,24 +6,32 @@
 */
 
 #ifndef MY_H_
-    #define MY_H_
+#define MY_H_
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-# include <sys/types.h>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
-# include <ctype.h>
-# include <dirent.h>
-# include <netinet/in.h>
-# include <arpa/inet.h>
+#include <netdb.h>
+
+void my_noop(int client);
+int password_check(int sock, FILE *need);
+void command_user(int sock);
+void my_quit(int client);
+char **my_tabb(char *tab);
+int user_check(int sock, FILE *need);
+void command_all(int client, char *recup);
+void login_user(int sock);
+void error(int ac, char **av);
+struct sockaddr_in complete_addr(char **av);
+void main_fonct(int sock, struct sockaddr_in addr);
+void complete_info(char **av);
+
 
 #endif /* !MY_H_ */
